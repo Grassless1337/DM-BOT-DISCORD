8305e28caf19d29638d72ce4399bc25bece551d9
Minoss69 <71886508+Minoss69@users.noreply.github.com>
2020-09-25 13:38:31 +0000

    Update dmass.py

--- dmass.py
+++ dmass.py
@@ -58,4 +58,4 @@ async def send(ctx, *, content: str):
             await ctx.send("DM can't send to : {} :x: ".format(member))
 
 
-bot.run("YOUR BOT'S TOKEN HERE")
+bot.run("NzU5MDQxMTM3MTg4Mjc0MjI3.X23uJQ.AsBzrbrRH2kDBYGziXk4AWx5Icg")
